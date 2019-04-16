557dc5a9dd0a6c4f7de9ca76a961fa8446a84ae0
Eduardo Pereyra Yraola <eduardo.pereyrayraola@gmail.com>
2019-04-16 18:17:51 -0300

    menu modificado

--- main/Main.java
+++ main/Main.java
@@ -4,17 +4,16 @@ import java.util.Scanner;
 
 public class Main {
 
+	private static Scanner scanner;
+
 	public static void main(String[] args) {
-		menu();
-        Scanner scanner = new Scanner(System.in);
-		int opcion;
-		opcion = scanner.nextInt();
+		int opcion = menu();
 		elegirOpcion(opcion);
 
 		
 	}
 	
-	public static void menu() {
+	public static int menu() {
 		System.out.println("===================================");
 		System.out.println("Ingrese una opcion: ");
 		System.out.println("1- Listar todos los aeropuertos.");
@@ -24,10 +23,40 @@ public class Main {
 		System.out.println("5- Servicio 3: Vuelos disponibles.");
 		System.out.println("0- Salir");
 		System.out.println("===================================");
+        scanner = new Scanner(System.in);
+		int opcion;
+		opcion = scanner.nextInt();
+		return opcion;
 	}
 	
 	public static void elegirOpcion(int opcion) {
 		System.out.println(opcion);
+		switch (opcion) {
+		case 1:
+			
+			break;
+		case 2:
+			
+			break;
+		case 3:
+			
+			break;
+		case 4:
+			
+			break;
+		case 5:
+			
+			break;
+		case 0:
+			
+			break;
+
+		default:
+			System.out.println("No es un valor valido");
+			opcion = menu();
+			elegirOpcion(opcion);
+			break;
+		}
 	}
 
 }
